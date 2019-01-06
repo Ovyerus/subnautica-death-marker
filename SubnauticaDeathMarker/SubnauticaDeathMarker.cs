69a154726a143e6c2b7a2b2e250436292cb5d863
Ovyerus <iamovyerus@gmail.com>
2019-01-06 18:36:45 +1100

    feat: delay spawning of marker by 5 sec so it doesn't obstruct player

--- SubnauticaDeathMarker/SubnauticaDeathMarker.cs
+++ SubnauticaDeathMarker/SubnauticaDeathMarker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Reflection;
+using System.Collections;
 using System.IO;
+using System.Reflection;
 using Harmony;
 using UnityEngine;
 using SMLHelper.V2.Handlers;
@@ -14,6 +15,7 @@ namespace SubnauticaDeathMarker
     {
         public static string InteractText = "RemoveDeathMarker";
         public static ConfigJson Config;
+        public static Vector3 DeathPosition;
 
         public static void Main()
         {
@@ -32,11 +34,22 @@ namespace SubnauticaDeathMarker
     [HarmonyPatch("OnKill")]
     [HarmonyPatch(new Type[] { typeof(DamageType) })]
     public class PlayerPatcher {
-        static void Postfix(DamageType damageType)
+        static void Prefix(DamageType damageType)
         {
-            Console.WriteLine("[DeathMarker] yeah he died lmao im dying xd");
-
             Camera playerCam = Player.main.camRoot.mainCam;
+            SubnauticaDeathMarker.DeathPosition = playerCam.transform.position;
+        }
+
+        static void Postfix()
+        {
+            Player.main.StartCoroutine(SpawnDeathMarker());
+        }
+
+        static IEnumerator SpawnDeathMarker()
+        {
+            // Delay spawning the marker so that it doesn't end up right in the player's face
+            yield return new WaitForSeconds(5);
+
             GameObject pingBase = new GameObject();
             GameObject pingModel = GameObject.Instantiate(Resources.Load<GameObject>("WorldEntities/Tools/DiveReelNode"));
             PingInstance ping = pingBase.AddComponent<PingInstance>();
@@ -51,12 +64,13 @@ namespace SubnauticaDeathMarker
             pingLight.type = LightType.Point;
             pingLight.range = 10f;
 
+            // Parent transforms
             pingModel.transform.SetParent(pingBase.transform);
             pingLight.transform.SetParent(pingModel.transform);
             pingCollider.transform.SetParent(pingModel.transform);
 
             // Set the ping to the player's position at death.
-            Vector3 playerPos = playerCam.transform.position;
+            Vector3 playerPos = SubnauticaDeathMarker.DeathPosition;
             pingBase.transform.position = playerPos;
 
             // Set information about the ping.
